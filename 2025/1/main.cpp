b45aacd26f6578c214780be2a68dcde9ca9894ef
6413 <56801084+6413@users.noreply.github.com>
2025-12-01 20:10:31 +0200

    Fix bug

--- 2025/1/main.cpp
+++ 2025/1/main.cpp
@@ -3,8 +3,8 @@
 import fan;
 
 int main() {
-  int current = 50, code = 0;
-  std::size_t found = 0, i = 0;
+  int current = 50, code = 0, i = -1;
+  std::size_t found = 0;
   std::string data = fan::io::file::read("input.txt");
   while ((found = data.find('\n', i = found + 1)) != std::string::npos) {
     code += (current += std::stoi(data.substr(i + 1)) * ((data[i] - 79) / 3)) % 100 == 0;
