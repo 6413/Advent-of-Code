48260daadc92b8da45f28f5a588e9856a5e1a698
6413 <56801084+6413@users.noreply.github.com>
2025-12-19 14:28:18 +0200

    Update part1.cpp

--- 2025/10/part1.cpp
+++ 2025/10/part1.cpp
@@ -4,7 +4,7 @@
 import fan;
 import fan.types.fstring;
 
-static constexpr uint8_t max_bits = 4;
+static constexpr uint8_t max_bits = sizeof(uint32_t) * 8;
 
 struct machine_t {
   std::bitset<max_bits> goal;
@@ -13,7 +13,7 @@ struct machine_t {
 };
 
 void parse(const std::string& data, std::vector<machine_t>& machines) {
-  fan::print("MUISTA VAIHTAA <N>\n");
+  //fan::print("MUISTA VAIHTAA <N>\n");
   auto lines = fan::split(data);
   for (const auto& line : lines) {
     std::size_t begin = line.find('[');
@@ -68,88 +68,31 @@ void parse(const std::string& data, std::vector<machine_t>& machines) {
     }
   }
 }
-
-void print(const std::vector<machine_t>& machines) {
-  for (const auto& machine : machines) {
-    fan::print(machine.goal);
-    fan::print("-----");
-    for (auto& mask : machine.wiring) {
-      fan::printr();
-      fan::printr(mask, "\n");
+uint32_t solve(const machine_t& machine) {
+  uint32_t shortest = -1;
+  for (uint32_t mask = 1; mask < 1 << machine.wiring.size(); ++mask) {
+    uint32_t count = 0;
+    uint32_t result = 0;
+    for (uint32_t bit = 0; bit < machine.wiring.size(); ++bit) {
+      (mask & (1 << bit)) && (result ^= machine.wiring[bit].to_ulong(), ++count, true);
     }
-    fan::print("-----");
-    fan::print(typename decltype(machine_t::wiring)::value_type(
-      0b11111
-    ));
-    fan::printr("{ ");
-    for (auto& joltage : machine.joltage_requirements) {
-      fan::printr(joltage, " ");
-    }
-    fan::print("}\n");
+    (result == machine.goal && count < shortest) && (shortest = count);
   }
+  return shortest;
 }
-
-std::vector<std::bitset<max_bits>> solve(machine_t& machine) {
-  uint32_t combinations = 1 << machine.wiring.size();
-  std::vector<std::bitset<max_bits>> shortest_combo;
-  uint32_t shortest_combo_counter = -1;
-  for (uint32_t mask = 1; mask < combinations; ++mask) {
-    std::vector<std::bitset<max_bits>> possible_combinations;
-    for (int bit = 0; bit < machine.wiring.size(); ++bit) {
-      if (mask & (1 << bit)) {
-        possible_combinations.emplace_back(machine.wiring[bit]);
-      }
-    }
-    std::bitset<max_bits> result;
-    for (const auto& bits : possible_combinations) {
-      result ^= bits;
-    }
-    if (result == machine.goal) {
-      if (possible_combinations.size() < shortest_combo_counter) {
-        shortest_combo = possible_combinations;
-        shortest_combo_counter = possible_combinations.size();
-      }
-    }
-    //for (auto& mask : possible_combinations) {
-    //  fan::print(mask);
-    //}
-   /* if (possible_combinations.size() == 2 && possible_combinations[1] == 0b1100) {
-      fan::print("\n");
-      fan::print(possible_combinations[0] ^ possible_combinations[1], machine.goal);
-      fan::print((result & machine.goal) == machine.goal, (result & machine.goal), possible_combinations.size());
-      fan::print("\n");
-    }*/
-    //fan::print("-----");
-    //fan::print(machine.goal);
-    fan::print("\n");
-  }
-  return shortest_combo;
-}
-
 
 int main() {
   std::string data = fan::io::file::read("input.txt");
   std::vector<machine_t> machines;
- 
-  parse(data, machines);
 
+  parse(data, machines);
   //print(machines);
 
+  fan::time::timer t {true};
   int64_t sum = 0;
-
-  for (auto [i, machine] : fan::enumerate(machines)) {
-    auto solved = solve(machine);
-    for (auto& bset : solved) {
-      fan::print(bset);
-    }
-    fan::print("-----");
-    fan::print(machine.goal);
-    sum += solved.size();
-    fan::print("presses:", solved.size());
-    fan::print("\n");
-    fan::print("---------------------------", i, "---------------------------");
+  for (const auto& machine : machines) {
+    sum += solve(machine);
   }
-
-
+  fan::print(t.elapsed() / 1e6);
   fan::print("total:", sum);
 }
